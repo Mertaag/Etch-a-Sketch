c76e5c6475ed0fa6980019ff06732036132ff1eb
Mertaag <141136569+Mertaag@users.noreply.github.com>
2024-03-04 17:01:18 -0500

    Add ability to chose side size

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 const container = document.querySelector('#container');
 let div;
+let sideSize = 100 //101;
 
-for(i=0; i<=255; i++){
+// while (sideSize>100 || sideSize == null || sideSize == ''){
+//     sideSize = prompt('Enter size size that is <=100');
+// }
+
+
+//create 16x16 grid
+for(i=0; i<=sideSize**2-1; i++){
     div = document.createElement('div');
     container.appendChild(div);
+    div.style.cssText = 'flex: 1 1 1%'; //how to calculate flex basis?
 }
 
+//paint squares
 container.addEventListener ('mouseover', (event) => {
     let target = event.target;
     target.style.backgroundColor = 'black';
 })
 
+const resetButton = document.querySelector('reset-button');
+resetButton.addEventListener ('click', () => {
+
+})
