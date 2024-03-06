1b22c378c3a17d07b089284c609cb903d4a0da45
Mertaag <141136569+Mertaag@users.noreply.github.com>
2024-03-06 13:01:34 -0500

    Implement reset button to create diff sideSize

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 const container = document.querySelector('#container');
 let div;
-let sideSize = 100 //101;
-let flexBasis = 100/sideSize;
+let sideSize;
 
-// while (sideSize>100 || sideSize == null || sideSize == ''){
-//     sideSize = prompt('Enter size size that is <=100');
-// }
+const resetButton = document.querySelector('#reset-button');
+resetButton.addEventListener ('click', () => {
+    sideSize = 120;
+    container.textContent = '';
+    
+    do {
+        sideSize = prompt('Enter size size that is <=100');
+    } while (sideSize>100 || sideSize == null || sideSize == '');
 
+    for(i=0; i<=sideSize**2-1; i++){
+        div = document.createElement('div');
+        container.appendChild(div);
+        div.style.cssText = `flex: 1 1 ${100/sideSize}%`;
+    }    
+})
 
-//create 16x16 grid
-for(i=0; i<=sideSize**2-1; i++){
-    div = document.createElement('div');
-    container.appendChild(div);
-    div.style.cssText = `flex: 1 1 ${flexBasis}%`;
-}
-
-//paint squares
 container.addEventListener ('mouseover', (event) => {
     let target = event.target;
     target.style.backgroundColor = 'black';
-})
-
-const resetButton = document.querySelector('reset-button');
-resetButton.addEventListener ('click', () => {
-
 })
